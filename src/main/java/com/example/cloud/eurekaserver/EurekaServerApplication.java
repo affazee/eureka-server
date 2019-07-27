194aa767cb07dcb81978a404b6e3b5bf21d52407
gyl <gaoyulu@126.com>
2019-07-27 18:45:57 +0800

    dgagg

--- src/main/java/com/example/cloud/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/example/cloud/eurekaserver/EurekaServerApplication.java
@@ -10,6 +10,7 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         System.out.println(1334);
+        System.out.println(23);
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
